490e0378df81e9efc1415e2f75394aa9f14a283e
Angyr-ool <dagbazhyk.a.s@gmail.com>
2024-06-04 13:35:29 +0300

    new launch settings added

--- Asp.Net.Core.6.WebApi/Program.cs
+++ Asp.Net.Core.6.WebApi/Program.cs
@@ -1,4 +1,24 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Configuration.AddJsonFile(opts =>
+{
+    if (builder.Environment.IsDevelopment())
+    {
+        opts.Path = "appsettings.Development.json";
+    }
+    else if (builder.Environment.IsStaging())
+    {
+        opts.Path = "appsettings.Staging.json";
+    }
+    else
+    {
+        opts.Path = "appsettings.json";
+    }
+
+    opts.Optional = false;
+    opts.ReloadOnChange = false;
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
